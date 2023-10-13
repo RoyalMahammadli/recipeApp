6fe98d5877741f88423837c093dd7bdb9c3a3253
Royal Mahammadli <rosqa_17@mail.ru>
2023-10-13 16:45:00 +0400

    testting2

--- src/component/ui/Card.jsx
+++ src/component/ui/Card.jsx
@@ -8,7 +8,7 @@ function Card({ foodName, img, ranking }) {
     const [modal2Open, setModal2Open] = useState(false);
 
     return (
-        <div className='card' onClick={() => setModal2Open(true)}>
+        <div className='card' onClick={() => setModal2Open(!modal2Open)}>
             <figure><img src={img} alt="" /></figure>
             <div className="text">
                 <h5>{foodName}</h5>
@@ -17,8 +17,8 @@ function Card({ foodName, img, ranking }) {
                     title="Vertically centered modal dialog"
                     centered
                     open={modal2Open}
-                    onOk={() => setModal2Open(false)}
-                    onCancel={() => setModal2Open(false)}
+                    onOk={() => setModal2Open(!modal2Open)}
+                    onCancel={() => setModal2Open(!modal2Open)}
                 >
                     <p>some contents...</p>
                     <p>some contents...</p>
